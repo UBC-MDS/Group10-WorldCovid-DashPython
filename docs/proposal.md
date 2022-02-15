934b6d7915e80f882a96226b2a92ddb7fd8b499c
Kingslin0810 <qinglin0810@gmail.com>
2022-02-15 15:54:09 -0800

    add team contract and re-structure repo folder

---
+++ docs/proposal.md
@@ -0,0 +1,12 @@
+# Covid-19 World Data Dashboard 
+
+## Motivation and purpose
+
+
+## Description of the data
+
+
+## Research questions and usage scenarios
+
+
+## Dashboard Design
---
+++ src/python/app.py
@@ -0,0 +1,8 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+import locale
+import altair as alt
+import datetime
